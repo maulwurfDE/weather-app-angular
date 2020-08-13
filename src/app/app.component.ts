5830794b61910796273f5997aaf1428dbcc16906
nearmint <mowmoh@gmail.com>
2020-08-13 16:39:56 +0200

    Add dynamic temperature and fahrenheit/celsius

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 import {spinnerWorks} from "./import.js"
 import { faSync } from '@fortawesome/free-solid-svg-icons';
 import { faTint, faWind } from '@fortawesome/free-solid-svg-icons';
@@ -15,17 +15,28 @@ export class AppComponent {
   formCountry: String;
   weatherState: any = "Nothing searched yet!";
   reloadSpinner: boolean = false;
-  // Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
+  filtered: any;
+  letterCelsius: boolean = true;
+  units: string = "metric";
+   // Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
 
   constructor() {
     // this.getWeather("münster","germany");
+
+    this.getWeather("Münster","Germany");
+ 
+ 
+
+
   }
+
+
 
 
   async getWeather(town,country) {
     // spinnerWorks.spin(document.getElementById('container'));
     this.reloadSpinner = true;
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${town},${country}&APPID=10e913130e75c308b518d5e8710fb645`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${town},${country}&APPID=10e913130e75c308b518d5e8710fb645&units=${this.units}`);
 
     const weatherData = await response.json();
 
@@ -33,19 +44,29 @@ export class AppComponent {
     
     // console.log(weatherData);
 
+
     let weatherArr = {clouds: weatherData.clouds.all, temp: weatherData.main.temp, place: weatherData.name, country: weatherData.sys.country}
 
     console.log(weatherArr);
 
     this.weatherState = weatherArr;
+    this.weatherState.temp = Math.round(this.weatherState.temp);
     await this.timeout(2000);
     this.reloadSpinner = false;
+
+
 
   }
 
   timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+
+
+
 }
+
+
+
 
 
   
